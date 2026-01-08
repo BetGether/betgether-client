7d950eb73823451d0450a8f71a198c383c3c6adb
YangJJune <ysj7191@naver.com>
2026-01-09 00:20:02 +0900

    style : Container 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 // import { useEffect } from "react";
 // import { getRanking } from "./apis/ranking";
 
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Routes";
+import Container from "./styles/Container";
+
 function App() {
   // 왜 사용되는지 이해가 안 되신다면, 아래 주석을 살리고 res 뒤에 .을 붙여서 내부 멤버들을 조회해보시기 바랍니다.
   // 참고로 당연하지만 아직 서버가 안 굴러가고 있어서 실행하면 오류가 납니다.
@@ -9,7 +13,13 @@ function App() {
   //     console.log(res);
   //   })
   // })
-  return <>BetGether</>;
+  return (
+    <BrowserRouter>
+      <Container>
+        <Router />
+      </Container>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/styles/Container.ts
@@ -0,0 +1,19 @@
+﻿import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 640px;
+  min-height: 100vh; // 전체 높이 확보
+  margin: 0 auto;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 가로 중앙 */
+  justify-content: flex-start; /* 세로는 위에서부터 시작 */
+  padding-top: 50px; /* 상단 여백만 살짝 추가 */
+`;
+
+export default Container;

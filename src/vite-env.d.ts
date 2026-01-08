7f68dd14bb2dabb07a7861b8a5ccd21e8a07d39b
YangJJune <ysj7191@naver.com>
2026-01-09 02:53:00 +0900

    fix : type 관련 에러 해결

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+﻿// src/vite-env.d.ts
+
+/// <reference types="vite/client" />
+
+declare module "*.svg?react" {
+  import React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
